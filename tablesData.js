e0d325ebe72dea8c2751f4767e27e0f7f0cbd872
Brad Nolan <bnolan56@gmail.com>
2018-08-13 19:43:00 -0600

    adding js

---
+++ tablesData.js
@@ -0,0 +1,25 @@
+// Exports for data arrays
+// =============================================================
+
+let confirmedTables = [
+  {
+    reservationName: '',
+    reservationEmail: '',
+    reservationID: '',
+    phoneNum: ''
+  }
+];
+
+let waitlistTable = [
+  {
+    reservationName: '',
+    reservationEmail: '',
+    reservationID: '',
+    phoneNum: ''
+  }
+];
+
+// Exports for data arrays
+// =============================================================
+module.exports = confirmedTables;
+module.exports = waitlistTable
