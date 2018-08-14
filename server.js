ae8c1532dc04b5e894960b14a8bfccb1e2d42ea3
Brad Nolan <bnolan56@gmail.com>
2018-08-13 19:26:38 -0600

    finishing server js

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 // Global Variables
+// =============================================================
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-
+// Sets up the Express App
+// =============================================================
 const app = express();
 const PORT = process.env.PORT || 3306;
 
-// ROUTES
-//goes here
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
 	console.log("Restaurant App listening on PORT: " + PORT);
 });
