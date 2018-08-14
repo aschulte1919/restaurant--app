480f873a7fd2cdfe4ca0c48b71f8802f61fe380c
Brad Nolan <bnolan56@gmail.com>
2018-08-13 19:09:23 -0600

    server js additions

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+// Global Variables
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
 
 const app = express();
-
 const PORT = process.env.PORT || 3306;
+
+// ROUTES
+//goes here
+
+app.listen(PORT, function() {
+	console.log("Restaurant App listening on PORT: " + PORT);
+});
